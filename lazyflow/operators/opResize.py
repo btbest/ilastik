661ff1fa24635ace32e1e26880f4b5ee5851cc05
Benedikt Best <63287233+btbest@users.noreply.github.com>
2025-02-25 10:32:12 +0100

    draft implementation

--- lazyflow/operators/opResize.py
+++ lazyflow/operators/opResize.py
@@ -19,28 +19,66 @@
 # This information is also available on the ilastik web site at:
 #          http://ilastik.org/license/
 ###############################################################################
-from collections import OrderedDict
+import logging
+
+import numpy as np
+from skimage import transform as sk_transform
 
 from lazyflow.graph import Operator, InputSlot, OutputSlot
+from lazyflow.roi import enlargeRoiForHalo, roiToSlice
+
+logger = logging.getLogger(__name__)
 
 
 class opResize(Operator):
+    """
+    Resizes input image to desired output size.
+    Cannot resize along channel axis (would be nonsense).
+    Time is treated like space axes, so resize along t at your own risk.
+
+    """
+
     RawImage = InputSlot()
-    TargetShape = InputSlot()
+    TargetShape = InputSlot()  # Tuple[int, ...]
     ResizedImage = OutputSlot()
 
     def setupOutputs(self):
-        assert len(self.RawImage.meta.shape) == len(
+        input_axiskeys = self.RawImage.meta.getAxisKeys()
+        assert len(input_axiskeys) == len(
             self.TargetShape.value
         ), f"Input image ({self.RawImage.meta.shape}) and target shape ({self.TargetShape.value}) must have same axes"
+        assert (
+            "c" not in input_axiskeys
+            or self.TargetShape.value[input_axiskeys.index("c")] == self.RawImage.meta.getTaggedShape["c"]
+        ), "Cannot resize along channel axis"
+        if (
+            "t" in input_axiskeys
+            and self.TargetShape.value[input_axiskeys.index("t")] != self.RawImage.meta.getTaggedShape["t"]
+        ):
+            logger.warning("Resizing along time axis. Are you sure this is what you want?")
         self.ResizedImage.meta.assignFrom(self.RawImage.meta)
         self.ResizedImage.meta.shape = self.TargetShape.value
 
     def execute(self, slot, subindex, roi, result):
         assert slot is self.ResizedImage, "Unknown output slot"
-        axes = self.RawImage.meta.getAxisKeys()
-        shape_in = self.RawImage.meta.getTaggedShape()
-        shape_out = self.ResizedImage.meta.getTaggedShape()
-        scaling_factors = OrderedDict([(a, shape_out[a] / shape_in[a]) for a in axes])
-        required_halo = (0 for _ in axes)  # TODO: Compute halo based on scaling factors
-        # roi_in = enlargeRoiForHalo(roi.start, roi.stop, shape_in.values(), sigma, window=3.5, enlarge_axes=None, return_result_roi=True)
+        shape_in = self.RawImage.meta.shape
+        shape_out = self.ResizedImage.meta.shape
+        # @haesleinhuepf sigmas for antialiasing (cf BioImageAnalysisNotebooks downscaling and denoising)
+        # scaling_factors = np.divide(shape_out, shape_in)
+        # sigmas = (1 / scale / 4 for scale in scaling_factors)
+        # skimage sigmas for antialiasing (cf skimage.transform.resize)
+        downsampling_factors = np.divide(shape_in, shape_out)
+        sigmas = np.maximum(0, (downsampling_factors - 1) / 2)
+        axes_to_enlarge = downsampling_factors > 1
+        roi_scaled_shape = np.divide(roi.stop - roi.start, downsampling_factors)
+        roi_with_halo, result_roi = enlargeRoiForHalo(
+            roi.start, roi.stop, shape_in.values(), sigma=sigmas, enlarge_axes=axes_to_enlarge, return_result_roi=True
+        )
+        raw_image = self.RawImage[roiToSlice(*roi_with_halo)].wait()
+        scaled_image = sk_transform.resize(
+            raw_image,
+            roi_scaled_shape,
+            anti_aliasing=True,
+            anti_aliasing_sigma=sigmas,
+        )
+        result[...] = scaled_image[roiToSlice(*result_roi)]
